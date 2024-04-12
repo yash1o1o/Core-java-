c1af656eea25d7537b4a85f2badb3cd3bc88b702
hp <hp@172.16.11.97>
2024-04-12 09:16:54 +0530

    first commit

---
+++ JN07Corejava/src/com/tnsif/dayone/Sample.java
@@ -0,0 +1,11 @@
+package com.tnsif.dayone;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System. out. println("My first java prog");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JN07Corejava/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module JN07Corejava {
+}
